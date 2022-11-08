ef8c5319bc72f6b403b2b0dcb5cd859cf64723e6
Camden Blatchly <camden.blatchly@ruralinnovation.us>
2022-11-08 09:59:24 -0700

    checkbox utility

--- js/index.js
+++ js/index.js
@@ -28,39 +28,40 @@ let race_color = {
   pop_native_hawaiian_pacific_islander_combo: hex_to_rgb_array("#7658A2")
 };
 
+function update_checkox(deckgl, dta, element_id, race_var) {
 
-function update_asian_checkbox() {
-  if (d3.select("#asian").property("checked")) {
-    race_filters.push('pop_asian_combo');
-    const layer = new deck.ScatterplotLayer({
-      data: map_dta.filter(d => race_filters.includes(d.variable)),
+  if (d3.select(element_id).property("checked")) {
+    race_filters.push(race_var);
+    const new_layer = new deck.ScatterplotLayer({
+      data: dta.filter(d => race_filters.includes(d.variable)),
       getPosition: (d) => [+d.lon, +d.lat],
       getColor: (d) => race_color[d.variable],
       getRadius: 300,
       opacity: 0.7
     })
-    deck.setProps([layer]);
 
-  } else {
-    const index = race_filters.indexOf('pop_asian_combo');
-    if (index > -1) { // only splice array when item is found
-      race_filters.splice(index, 1); // 2nd parameter means remove one item only
-      const layer = new deck.ScatterplotLayer({
-        data: map_dta.filter(d => race_filters.includes(d.variable)),
-        getPosition: (d) => [+d.lon, +d.lat],
-        getColor: (d) => race_color[d.variable],
-        getRadius: 300,
-        opacity: 0.7
-      })
-      deck.setProps([layer]);
-    }
+    deckgl.setProps({ layers:  new_layer  });
   }
+  else {
+    let var_index = race_filters.indexOf(race_var);
+    if (var_index > -1) {race_filters.splice(var_index, 1)};
+    const new_layer = new deck.ScatterplotLayer({
+      data: dta.filter(d => race_filters.includes(d.variable)),
+      getPosition: (d) => [+d.lon, +d.lat],
+      getColor: (d) => race_color[d.variable],
+      getRadius: 300,
+      opacity: 0.7
+    })
+
+    deckgl.setProps({ layers:  new_layer  });
+  }
+
 }
 
 function render() {
 
     Promise.all([
-      d3.csv("data/dots_100p_all_rural_2020.csv")
+      d3.csv("https://rwjf-public.s3.amazonaws.com/dots_100p_all_rural_shuffled_2020.csv")
     ]).then(function(files) {
 
       const deckgl = new deck.DeckGL({
@@ -90,33 +91,34 @@ function render() {
 
       deckgl.setProps({ layers:  new_layer  });
 
+      d3.select("#asian")
+        .on("change", function(d) {
+          update_checkox(deckgl, files[0], "#asian", "pop_asian_combo");
+        });
+
+      d3.select("#aian")
+        .on("change", function(d) {
+          update_checkox(deckgl, files[0], "#aian", "pop_american_indian_combo");
+        });
+
+      d3.select("#black")
+        .on("change", function(d) {
+          update_checkox(deckgl, files[0], "#black", "pop_black_combo");
+        });
+
+      d3.select("#hispanic-latino")
+        .on("change", function(d) {
+          update_checkox(deckgl, files[0], "#hispanic-latino", "pop_hispanic_combo");
+        });
+
+      d3.select("#nhpi")
+        .on("change", function(d) {
+          update_checkox(deckgl, files[0], "#nhpi", "pop_native_hawaiian_pacific_islander_combo");
+        });
+
       d3.select("#white")
         .on("change", function(d) {
-          if (d3.select("#white").property("checked")) {
-            race_filters.push("pop_white_combo");
-            const new_layer = new deck.ScatterplotLayer({
-              data: files[0].filter(d => race_filters.includes(d.variable)),
-              getPosition: (d) => [+d.lon, +d.lat],
-              getColor: (d) => race_color[d.variable],
-              getRadius: 300,
-              opacity: 0.7
-            })
-
-            deckgl.setProps({ layers:  new_layer  });
-          }
-          else {
-            let var_index = race_filters.indexOf("pop_white_combo");
-            if (var_index > -1) {race_filters.splice(var_index, 1)};
-            const new_layer = new deck.ScatterplotLayer({
-              data: files[0].filter(d => race_filters.includes(d.variable)),
-              getPosition: (d) => [+d.lon, +d.lat],
-              getColor: (d) => race_color[d.variable],
-              getRadius: 300,
-              opacity: 0.7
-            })
-
-            deckgl.setProps({ layers:  new_layer  });
-          }
+          update_checkox(deckgl, files[0], "#white", "pop_white_combo");
         });
 
 
